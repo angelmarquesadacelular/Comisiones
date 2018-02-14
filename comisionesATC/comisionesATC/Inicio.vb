2beaac2c1bf9c2da066a999e5e585a751c21c5ad
angel campos <angelcamposalejo@gmail.com>
2018-02-14 15:43:38 -0600

    validaciones formulario LoginDlg

--- comisionesATC/comisionesATC/Inicio.vb
+++ comisionesATC/comisionesATC/Inicio.vb
@@ -17,4 +17,11 @@
     Private Sub LoginToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoginToolStripMenuItem.Click
         LoginDlg.Visible = True
     End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        EstadísticasToolStripMenuItem.Enabled = False
+        CargarReportesToolStripMenuItem.Enabled = False
+        SalirToolStripMenuItem.Enabled = False
+        LoginToolStripMenuItem.Enabled = True
+    End Sub
 End Class
--- comisionesATC/comisionesATC/LoginDlg.vb
+++ comisionesATC/comisionesATC/LoginDlg.vb
@@ -5,21 +5,31 @@
     End Sub
     'Método que permite acceder al funcionamiento del sistema
     Private Sub AccesarSistema()
-        'Encriptación del password del usuario
-        Dim passwordEncriptado = EncriptarPassword(TbxPassword.Text)
-        'Se obtiene el acceso del usuario
-        permiso_id = AccesoUsuario(TbxUsuario.Text, passwordEncriptado)
-        'Se verifica que el usuario tenga permiso de accesar al sistema
-        If permiso_id > 0 Then
-            Limpiarformulario()
-            Inicio.EstadísticasToolStripMenuItem.Enabled = True
-            Inicio.CargarReportesToolStripMenuItem.Enabled = True
-            Inicio.SalirToolStripMenuItem.Enabled = True
-            Inicio.LoginToolStripMenuItem.Enabled = False
-            Me.Visible = False
+        If TbxUsuario.Text.Length() > 0 Then
+            If TbxPassword.Text.Length() > 0 Then
+                'Encriptación del password del usuario
+                Dim passwordEncriptado = EncriptarPassword(TbxPassword.Text)
+                'Se obtiene el acceso del usuario
+                permiso_id = AccesoUsuario(TbxUsuario.Text, passwordEncriptado)
+                'Se verifica que el usuario tenga permiso de accesar al sistema
+                If permiso_id > 0 Then
+                    Limpiarformulario()
+                    Inicio.EstadísticasToolStripMenuItem.Enabled = True
+                    Inicio.CargarReportesToolStripMenuItem.Enabled = True
+                    Inicio.SalirToolStripMenuItem.Enabled = True
+                    Inicio.LoginToolStripMenuItem.Enabled = False
+                    Me.Visible = False
+                Else
+                    MsgBox("Datos de acceso incorrectos", MsgBoxStyle.Information, Title:="¡¡ATENCIÓN!!")
+                    Limpiarformulario()
+                End If
+            Else
+                MsgBox("Favor de teclear el password de acceso", MsgBoxStyle.Information, Title:="¡¡ATENCIÓN!!")
+                TbxPassword.Select()
+            End If
         Else
-            MsgBox("Datos de acceso incorrectos", MsgBoxStyle.Information, Title:="¡¡ATENCIÓN!!")
-            Limpiarformulario()
+            MsgBox("Favor de teclear el usuario de acceso", MsgBoxStyle.Information, Title:="¡¡ATENCIÓN!!")
+            TbxUsuario.Select()
         End If
     End Sub
 
@@ -30,5 +40,26 @@
 
     Private Sub LoginDlg_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         permiso_id = 0
+    End Sub
+
+    Private Sub TbxUsuario_KeyDown(sender As Object, e As KeyEventArgs) Handles TbxUsuario.KeyDown
+        Select Case e.KeyData
+            Case Keys.Enter
+                AccesarSistema()
+        End Select
+    End Sub
+
+    Private Sub TbxPassword_KeyDown(sender As Object, e As KeyEventArgs) Handles TbxPassword.KeyDown
+        Select Case e.KeyData
+            Case Keys.Enter
+                AccesarSistema()
+        End Select
+    End Sub
+
+    Private Sub LoginDlg_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        Select Case e.KeyData
+            Case Keys.Enter
+                AccesarSistema()
+        End Select
     End Sub
 End Class
