3c940aaa0e77b525e96aaf83e096cfd8e963b215
jesus2708 <ti11110159@itess.edu.mx>
2018-02-14 15:03:05 -0600

    reportes de utilidad y comisionesw

--- comisionesATC/comisionesATC/reporte/Estadisticas.vb
+++ comisionesATC/comisionesATC/reporte/Estadisticas.vb
@@ -18,9 +18,6 @@
                 Next
             End If
 
-            'For Each dgr1 As DataGridViewRow In ReportesEstadisticosDlg.DGReporteMenor.Rows
-            '    dt.Rows.Add(dgr1.Cells(0).Value, dgr1.Cells(1).Value, dgr1.Cells(2).Value, dgr1.Cells(3).Value)
-            'Next
             ds.Tables.Add(dt)
             ds.WriteXmlSchema("%deActivacionesmas.xml")
             'Try
@@ -29,26 +26,56 @@
             CREstadisticas.ReportSource = cr
             CREstadisticas.Refresh()
 
+        ElseIf ReportesEstadisticosDlg.CBTipoReporte.Text = "Comisiones" Then
+            Dim ds As New DataSet()
+            Dim dt As New DataTable()
+            dt.Columns.Add("Cliente", GetType(String))
+            dt.Columns.Add("Total", GetType(Int16))
 
 
+            If ReportesEstadisticosDlg.TabReporte.SelectedIndex = 0 Then
+                For Each dgr As DataGridViewRow In ReportesEstadisticosDlg.DGReporteMayor.Rows
+                    dt.Rows.Add(dgr.Cells(0).Value, dgr.Cells(1).Value)
+                Next
+            Else
+                For Each dgr As DataGridViewRow In ReportesEstadisticosDlg.DGReporteMenor.Rows
+                    dt.Rows.Add(dgr.Cells(0).Value, dgr.Cells(1).Value)
+                Next
+            End If
+
+            ds.Tables.Add(dt)
+            ds.WriteXmlSchema("Comisiones.xml")
+            'Try
+            Dim cr = New Comisiones()
+            cr.SetDataSource(ds)
+            CREstadisticas.ReportSource = cr
+            CREstadisticas.Refresh()
+
+        ElseIf ReportesEstadisticosDlg.CBTipoReporte.Text = "Utilidad" Then
+            Dim ds As New DataSet()
+            Dim dt As New DataTable()
+            dt.Columns.Add("Cliente", GetType(String))
+            dt.Columns.Add("Recargas", GetType(Int16))
+            dt.Columns.Add("Utilidad", GetType(Double))
 
 
-            'dt1.Columns.Add("Cliente", GetType(String))
-            'dt1.Columns.Add("Tota de Numeros", GetType(Int16))
-            'dt1.Columns.Add("Activos", GetType(Int16))
-            'dt1.Columns.Add("% de Activados", GetType(Double))
+            If ReportesEstadisticosDlg.TabReporte.SelectedIndex = 0 Then
+                For Each dgr As DataGridViewRow In ReportesEstadisticosDlg.DGReporteMayor.Rows
+                    dt.Rows.Add(dgr.Cells(0).Value, dgr.Cells(1).Value, dgr.Cells(2).Value)
+                Next
+            Else
+                For Each dgr As DataGridViewRow In ReportesEstadisticosDlg.DGReporteMenor.Rows
+                    dt.Rows.Add(dgr.Cells(0).Value, dgr.Cells(1).Value, dgr.Cells(2).Value)
+                Next
+            End If
 
-
-            'For Each dgr1 As DataGridViewRow In ReportesEstadisticosDlg.DGReporteMenor.Rows
-            '    dt1.Rows.Add(dgr1.Cells(0).Value, dgr1.Cells(1).Value, dgr1.Cells(2).Value, dgr1.Cells(3).Value)
-            'Next
-            'ds1.Tables.Add(dt1)
-            'ds1.WriteXmlSchema("%deActivacionesmenos.xml")
-            'Dim cr1 = New porcentaje()
-            'cr1.SetDataSource(ds1)
-            'CREstadisticas.ReportSource = cr1
-            'CREstadisticas.Refresh()
-
+            ds.Tables.Add(dt)
+            ds.WriteXmlSchema("Utilidad.xml")
+            'Try
+            Dim cr = New Utilidad()
+            cr.SetDataSource(ds)
+            CREstadisticas.ReportSource = cr
+            CREstadisticas.Refresh()
         End If
     End Sub
 
